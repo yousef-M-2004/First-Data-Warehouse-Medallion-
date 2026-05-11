a0ddcbc8f5c7c1961a25ffe39a68081aae1ed793
yousef mohamed <y.abdalla2611@gmail.com>
2026-05-11 21:27:24 +0300

    Update and rename meme to DDL_silver.sql

--- scripts/silver/DDL_silver.sql
+++ scripts/silver/DDL_silver.sql
@@ -1,5 +1,16 @@
+/*
+running this script will drop the crm and the erp silver layered files and recreate it aging.
+it takes the data from the bronze layer and load it in the silver layer after doing some
+cleaning to it so make sure to run the bronze layer first to insure a high quality data.
+*/
+
+
+
 IF OBJECT_ID ('silver.crm_cust_info','U') IS NOT NULL
 	DROP TABLE silver.crm_cust_info;
+
+GO 
+	
 create table silver.crm_cust_info (
 cst_id INT ,
 cst_key NVARCHAR(50),
@@ -11,6 +22,8 @@ cst_create_date DATE,
 dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
+GO 
+	
 IF OBJECT_ID ('silver.crm_prd_info','U') IS NOT NULL
 	DROP TABLE silver.crm_prd_info;
 CREATE TABLE silver.crm_prd_info (
@@ -23,6 +36,8 @@ prd_start_dt DATETIME,
 prd_end_dt DATETIME,
 dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
+
+GO
 
 IF OBJECT_ID ('silver.crm_sales_details','U') IS NOT NULL
 	DROP TABLE silver.crm_sales_details;
@@ -39,6 +54,8 @@ sls_price INT,
 dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
+GO
+
 IF OBJECT_ID ('silver.erp_loc_a101','U') IS NOT NULL
 	DROP TABLE silver.erp_loc_a101;
 CREATE TABLE silver.erp_loc_a101 (
@@ -46,6 +63,8 @@ cid NVARCHAR(50),
 cntry NVARCHAR(50),
 dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
+
+GO
 
 IF OBJECT_ID ('silver.erp_cust_az12','U') IS NOT NULL
 	DROP TABLE silver.erp_cust_az12;
@@ -55,6 +74,8 @@ bdate DATE,
 gen NVARCHAR(50),
 dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
+
+GO
 
 IF OBJECT_ID ('silver.erp_px_cat_g1v2','U') IS NOT NULL
 	DROP TABLE silver.erp_px_cat_g1v2;
